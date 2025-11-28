5779577bddff10980485dc34033bfb435ffa3c8b
Aksh Rawat <akshrawat88888888@gmail.com>
2025-11-28 13:22:21 +0530

    Add new exercise

---
+++ Exercise-9.js
@@ -0,0 +1,45 @@
+let Adjectives={
+    0:"Ivory",
+    1:"Crazy",
+    2:" Buzz",
+    3:"Crunch",
+    4:"Crunch",
+    5:"Royal",
+    6:"Elite",
+    7:"Prestige",
+    8:"Pure",
+    9:"Shaft",
+    10:"Show"
+}
+let shopName={
+    0:"Silk",
+    1:"Engine",
+    2:"Food",
+    3:"Milk",
+    4:"Sugar",
+    5:"Accessories",
+    6:"Electrical",
+    7:"Chic",
+    8:"Nova",
+    9:"Aura",
+    10:"Atomic"
+}
+let anotherWord={
+    0:"Fusion",
+    1:"Bros",
+    2:"limited",
+    3:"Hub",
+    4:"Studio",
+    5:"Flair",
+    6:"Bloom",
+    7:"Charm",
+    8:"Dots",
+    9:"Market",
+    10:"Cluster"
+}
+
+let math1=Math.floor(Math.random()*10)
+let math2=Math.floor(Math.random()*10)
+let math3=Math.floor(Math.random()*10)
+
+console.log(Adjectives[math1]+"-"+shopName[math2]+"-"+anotherWord[math3])
